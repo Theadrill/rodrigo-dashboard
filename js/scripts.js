61fd514fef5b4fa64547034152cfe56234c70847
Theadrill <theadrill@gmail.com>
2025-01-31 11:52:23 -0300

    ups

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,37 @@ function openFullscreen() {
         }
     }
 }
+
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false)
+
+document.addEventListener("keydown", function onPress(event) {
+    switch (event.key) {
+        case "ArrowLeft":
+            
+            break
+        case "ArrowRight":
+            
+            break
+        case "ArrowUp":
+            //.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowUp'}))
+            
+            //console.log(document.getElementById('light').contentWindow)
+
+            // console.log(document.embeds[1])
+
+            // document.getElementById('lightEmbed').click()
+
+            //.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowUp'}))
+
+            
+            break
+        case "ArrowDown":
+            
+            break
+    }
+})
